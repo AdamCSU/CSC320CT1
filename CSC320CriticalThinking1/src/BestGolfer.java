480934b8c2b047f1e639e092203f4b1387d7d09d
Adam Gorry <adam.gorry@csuglobal.edu>
2024-08-25 20:11:54 -0600

    Initial commit.

---
+++ CSC320CriticalThinking1/src/BestGolfer.java
@@ -0,0 +1,25 @@
+/*Colorado State University Global 
+#24SD-CSC320-2: Programming 1
+#Dr. Douglas Mujeye
+#August 25, 2024*/
+
+public class BestGolfer {
+
+	public static void main(String[] args) {
+		//Declare and initialize the variables
+		String firstName = "Tiger";
+		String lastName = "Woods";
+		String streetAddress = "123 Goat Dr.";
+		String city = "Augusta";
+		int zipcode = 12345;
+		
+		//Print out each variable on its own line
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAddress);
+		System.out.println(city);
+		System.out.println(zipcode);
+
+	}
+
+}
